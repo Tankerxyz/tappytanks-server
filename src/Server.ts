2f682dcd2657b402fe6d6fffb9801bb28e315bef
Tankerxyz <tankerxyz1@gmail.com>
2019-02-28 14:35:18 +0200

    add using db-uri preconfigured from local .env file

--- src/Server.ts
+++ src/Server.ts
@@ -37,7 +37,7 @@ export default class Server {
   }
 
   private configureDb(): void {
-    mongoose.connect('mongodb://localhost/tappytanks', {useNewUrlParser: true});
+    mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
     this.db = mongoose.connection;
     this.db.on('error', console.error.bind(console, 'connection error:'));
     this.db.once('open', () => {
