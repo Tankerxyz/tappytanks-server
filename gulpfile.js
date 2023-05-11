0eab01e2a00d6d7831cff367ea94683d2a1c6ad6
Tankerxyz <tankerxyz1@gmail.com>
2023-05-11 23:33:27 +0300

    fix typescript build and mongo connection

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-const tsProject = ts.createProject('tsconfig.json', { rootDir: 'src', outDir: 'dist' });
 
-gulp.task('build', function () {
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('build', function() {
   return tsProject.src()
     .pipe(tsProject())
-    .pipe(gulp.dest('./dist'));
+    .js.pipe(gulp.dest('dist'));
 });
 
-//set up a watcher to watch over changes
-gulp.task('watch', gulp.series('build', () => {
-  gulp.watch(tsProject.config.files)
-    .on('change', gulp.series('build'));
-}));
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.ts', gulp.series('build'));
+});
 
-gulp.task('default', gulp.series('watch'));
+gulp.task('default', gulp.series('build', 'watch'));
--- src/Server.ts
+++ src/Server.ts
@@ -37,7 +37,8 @@ export default class Server {
   }
 
   private configureDb(): void {
-    mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
+
+    mongoose.connect(process.env.DB_URI);
     this.db = mongoose.connection;
     this.db.on('error', console.error.bind(console, 'connection error:'));
     this.db.once('open', () => {
